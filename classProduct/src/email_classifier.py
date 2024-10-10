e97bf1a0379159c6d8e356332fbd38ba9c2ce69e
Patrick Vorreiter <patrick.vorreiter@gmx.de>
2024-10-10 14:58:04 +0100

    Update email_classifier.py

--- classProduct/src/email_classifier.py
+++ classProduct/src/email_classifier.py
@@ -1,16 +1,17 @@
-from preprocessing import DataProcessor
-from dataset_loader import DatasetLoader
+from DataPreparation.preprocessing import DataProcessor
+from DataPreparation.dataset_loader import DatasetLoader
+from FeatureEngineering.feature_engineering import FeatureEngineer
 from training_data import TrainingData
-from randomforest import RandomForest
+from Models.randomforest import RandomForest
 import pandas as pd
 
 
 class EmailClassifier():
 
     def __init__(self) -> None:
-
-        self.data_processor: DataProcessor = None
         self.data_set_loader: DatasetLoader = DatasetLoader()
+        self.data_processor: DataProcessor = None
+        self.feature_engineer: FeatureEngineer = None
         self.model: RandomForest = None
         self.df: pd.DataFrame = None
         self.data: TrainingData = None
@@ -25,11 +26,24 @@ class EmailClassifier():
         return classification
 
     def train_model(self, path: str) -> None:
+        # load the data
         self.df = self.data_set_loader.read_data(path)
         self.df = self.data_set_loader.renameColumns(self.df)
+
+        # preproccess the data
         self.data_processor = DataProcessor(self.df)
-        self.data_processor.create_tfidf_embd()
-        X = self.data_processor.get_tfidf_embd()
+        self.dataset_processor.de_duplication()
+        self.dataset_processor.translate_to_en()
+        self.dataset_processor.noise_remover()
+        self.dataset_processor.convert_to_unicode()
+        self.df = self.dataset_processor.get_df()
+
+        # feature engineering
+        self.feature_engineer = FeatureEngineer(self.df)
+        self.feature_engineer.create_tfidf_embd()
+        X = self.feature_engineer.get_tfidf_embd()
+
+        # modelling
         self.data = TrainingData(X, self.df)
         self.model = RandomForest(
             'RandomForest', self.data.get_X_test(), self.data.get_type())
