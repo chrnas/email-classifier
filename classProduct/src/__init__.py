22a4177d995f49e49417ae1d8214501d327be0b7
chris <chrna581@student.liu.se>
2024-11-02 14:19:22 +0000

    Added code for UML diagrams

--- classProduct/src/__init__.py
+++ classProduct/src/__init__.py
@@ -1,5 +1,5 @@
 from .models import base
 from .models import randomforest
 from .feature_engineering import feature_engineering
-from .data_preparation import preprocessing
+from .data_preparation import data_processor
 from .data_preparation import data_preparation
