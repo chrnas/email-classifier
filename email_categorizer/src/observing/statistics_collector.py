a7729042a29e1f4ff8e058abeb4be8fc3fb634b0
selintaskin <73575125+selintaskin@users.noreply.github.com>
2024-11-21 12:31:08 +0000

    Update statistics_collector.py

--- email_categorizer/src/observing/statistics_collector.py
+++ email_categorizer/src/observing/statistics_collector.py
@@ -1,7 +1,25 @@
-
 from .observe import Observer
+from context_classification.context import ContextClassifier
 
-class StatCollector (Observer):
+class StatCollector(Observer):
     
-    def get_scores():
-        pass
+    def __init__(self):
+        self.statistics = None
+
+    def update(self, event_type, statistics):
+        if event_type != 'evaluating':
+            return  # Ignore irrelevant eventspython
+        # Handle relevant event
+        self.statistics = statistics
+        self.display()
+
+    def display(self):
+        # Print collected statistics
+         for email_class, met in self.statistics.items():
+            if email_class not in ["accuracy", "macro avg", "weighted avg"]:
+                print(f"Email Type: {email_class}")
+                print(f"Precision: {met['precision']}")
+                print(f"Recall: {met['recall']}")
+                print(f"F1-Score: {met['f1-score']}")
+                print(f"Support: {met['support']}")
+                print("\n")
