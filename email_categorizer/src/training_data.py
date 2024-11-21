d27d08068cc0b28fd46aca7693e4a9a28f6ad76f
Patrick Vorreiter <patrick.vorreiter@gmx.de>
2024-11-21 15:21:21 +0000

    Update training_data.py

--- email_categorizer/src/training_data.py
+++ email_categorizer/src/training_data.py
@@ -1,19 +1,11 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-import random
-
-seed = 0
-random.seed(seed)
-np.random.seed(seed)
-
 
 class TrainingData():
     def __init__(self,
                  X: np.ndarray,
                  df: pd.DataFrame) -> None:
-        # This method will create the model for data
-        # This will be performed in second activity
         self.y = df.y.to_numpy()
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
             X, df.y, test_size=0.2, random_state=0)
