3bb1c909de5b9dac10d0b88c588ddb7486bc0645
Patrick Vorreiter <patrick.vorreiter@gmx.de>
2024-11-21 13:12:14 +0000

    Update observe.py

--- email_categorizer/src/observing/observe.py
+++ email_categorizer/src/observing/observe.py
@@ -1,4 +1,11 @@
+from context_classification.context import ContextClassifier
+from abc import ABC, abstractmethod
 
-class Observer:
-    def observe():
-        pass
+class Observer(ABC):
+    
+    @abstractmethod
+    def update(self,event_type ,data):
+        ...
+
+    def display(self):
+        ...
