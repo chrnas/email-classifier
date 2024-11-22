3b7015c5a7c2314ad00d75a49613b43dea221c6d
Patrick Vorreiter <patrick.vorreiter@gmx.de>
2024-11-22 10:38:30 +0000

    Fix SVC

--- email_categorizer/src/models/svc.py
+++ email_categorizer/src/models/svc.py
@@ -3,9 +3,8 @@ from .base import BaseModel
 
 
 class SVCModel(BaseModel):
-    def __init__(self,
-                 model_name: str) -> None:
-        super().__init__(model_name)
+    def __init__(self) -> None:
+        super().__init__()
         self.mdl = SVC()
 
     def __str__(self) -> str:
