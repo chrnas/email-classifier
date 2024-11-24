526b36ff6816da5a60e1fbb6b9cf497aac30e72e
Christoffer Näs <christoffernas2001@gmail.com>
2024-11-24 19:07:09 +0000

    Removed all warning messages on startup

--- email_categorizer/src/data_preparation/data_processor.py
+++ email_categorizer/src/data_preparation/data_processor.py
@@ -62,26 +62,26 @@ class NoiseRemovalDecorator(DataProcessorDecorator):
             df)  # Get the current DataFrame from the previous processor
 
         # Define the noise patterns for 'Ticket Summary'
-        noise = "(sv\s*:)|(wg\s*:)|(ynt\s*:)|(fw(d)?\s*:)|(r\s*:)|(re\s*:)|(\[|\])|(aspiegel support issue submit)|(null)|(nan)|((bonus place my )?support.pt 自动回复:)"
+        noise = r"(sv\s*:)|(wg\s*:)|(ynt\s*:)|(fw(d)?\s*:)|(r\s*:)|(re\s*:)|(\[|\])|(aspiegel support issue submit)|(null)|(nan)|((bonus place my )?support.pt 自动回复:)"
         df[self.config.ticket_summary] = df[self.config.ticket_summary].str.lower().replace(
             noise, " ", regex=True).replace(r'\s+', ' ', regex=True).str.strip()
 
         # Define additional noise patterns for 'Interaction content'
         noise_1 = [
-            "(from :)|(subject :)|(sent :)|(r\s*:)|(re\s*:)", "(january|february|march|april|may|june|july|august|september|october|november|december)",
-            "(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)", "(monday|tuesday|wednesday|thursday|friday|saturday|sunday)", "\d{2}(:|.)\d{2}",
-            "(xxxxx@xxxx\.com)|(\*{5}\([a-z]+\))", "dear ((customer)|(user))", "dear", "(hello)|(hallo)|(hi )|(hi there)",
-            "good morning", "thank you for your patience ((during (our)? investigation)|(and cooperation))?",
-            "thank you for contacting us", "thank you for your availability", "thank you for providing us this information",
-            "thank you for contacting", "thank you for reaching us (back)?", "thank you for patience", "thank you for (your)? reply",
-            "thank you for (your)? response", "thank you for (your)? cooperation", "thank you for providing us with more information",
-            "thank you very kindly", "thank you( very much)?", "i would like to follow up on the case you raised on the date",
-            "i will do my very best to assist you", "in order to give you the best solution", "could you please clarify your request with following information:",
-            "in this matter", "we hope you(( are)|('re)) doing ((fine)|(well))", "i would like to follow up on the case you raised on",
-            "we apologize for the inconvenience", "sent from my huawei (cell )?phone", "original message", "customer support team",
-            "(aspiegel )?se is a company incorporated under the laws of ireland with its headquarters in dublin, ireland.",
-            "(aspiegel )?se is the provider of huawei mobile services to huawei and honor device owners in",
-            "canada, australia, new zealand and other countries", "\d+", "[^0-9a-zA-Z]+", "(\s|^).(\s|$)"
+            r"(from :)|(subject :)|(sent :)|(r\s*:)|(re\s*:)", r"(january|february|march|april|may|june|july|august|september|october|november|december)",
+            r"(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)", r"(monday|tuesday|wednesday|thursday|friday|saturday|sunday)", r"\d{2}(:|.)\d{2}",
+            r"(xxxxx@xxxx\.com)|(\*{5}\([a-z]+\))", r"dear ((customer)|(user))", "dear", r"(hello)|(hallo)|(hi )|(hi there)",
+            r"good morning", r"thank you for your patience ((during (our)? investigation)|(and cooperation))?",
+            r"thank you for contacting us", r"thank you for your availability", r"thank you for providing us this information",
+            r"thank you for contacting", r"thank you for reaching us (back)?", r"thank you for patience", r"thank you for (your)? reply",
+            r"thank you for (your)? response", r"thank you for (your)? cooperation", r"thank you for providing us with more information",
+            r"thank you very kindly", r"thank you( very much)?", r"i would like to follow up on the case you raised on the date",
+            r"i will do my very best to assist you", r"in order to give you the best solution", r"could you please clarify your request with following information:",
+            r"in this matter", r"we hope you(( are)|('re)) doing ((fine)|(well))", r"i would like to follow up on the case you raised on",
+            r"we apologize for the inconvenience", r"sent from my huawei (cell )?phone", r"original message", "customer support team",
+            r"(aspiegel )?se is a company incorporated under the laws of ireland with its headquarters in dublin, ireland.",
+            r"(aspiegel )?se is the provider of huawei mobile services to huawei and honor device owners in",
+            r"canada, australia, new zealand and other countries", r"\d+", "[^0-9a-zA-Z]+", r"(\s|^).(\s|$)"
         ]
 
         # Apply noise removal to 'Interaction content' column
